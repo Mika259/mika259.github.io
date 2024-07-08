713efe10352bb39ebd61b0481ed61cd0932f4e92
M1K259 <87774553+Mika259@users.noreply.github.com>
2024-07-09 04:21:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 
 function cp(){
     var text = window.location.href
-    navigator.clipboard.writeText(text)
+    navigator.clipboard.writeText(text.value)
 
-    window.alert("Copied url : "+text)
+    window.alert("Copied url : "+text.value)
 
     }
